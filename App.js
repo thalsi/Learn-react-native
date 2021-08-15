85f0365fe3f5239b6304bebcf0db32a0281a2992
thalsi <thalseehthalsi609@gmail.com>
2021-08-15 13:08:37 +0530

    states and props

--- App.js
+++ App.js
@@ -1,12 +1,109 @@
-import React from 'react';
-import { Text, View } from 'react-native';
 
-const Wolad = () => {
-    return (
+// 1. function componet
+
+// import React from 'react';
+// import { Text } from 'react-native';
+
+// const HelloFunction =()=>{
+//     return (
+//         <Text>Hello world.. thalseeh</Text>
+//     );
+// }
+
+// export default HelloFunction;
+
+
+
+
+
+
+//2 class componet
+
+// import React,{ Component } from 'react';
+// import { Text } from 'react-native';
+
+
+// class HelloClass extends Component {
+//     render(){
+//         return (
+//             <Text>hello calss componet</Text>
+//         );
+//     }
+// }
+
+// export default HelloClass;
+
+
+
+
+
+
+
+//2. Custom Components
+
+// import React from 'react';
+// import {View,Text,TextInput} from 'react-native';
+
+// const HelloCustom =()=>{
+
+//     return(
+//         <View>
+//             <Text>Hello World..!s</Text>
+//             <TextInput
+//             style={{
+//                 height:40,
+//                 borderColor:'green',
+//                 borderWidth:1,
+//             }}
+//             defaultValue="Enter Value"></TextInput>
+//         </View>
+//     );
+// }
+
+// export default HelloCustom;
+
+
+
+
+
+//4 .multiple components
+
+import React, { useState } from 'react';
+import {View, Text, Image, Button} from 'react-native'
+
+const Detials=(props)=>{
+    const [value,setValue]=useState(false);
+    return(
         <View>
-            <Text>Hello world..!</Text>
+            <Text>Hello Detials page { props.name }</Text>
+            <Image
+            source={{uri:'https://analyticsindiamag.com/wp-content/uploads/2020/10/7d744a684fe03ebc7e8de545f97739dd.jpg'}}
+            style={{
+                width:100,
+                height:100
+            }}
+            >
+            </Image>
+            <Button
+            onPress={()=>{
+                setValue(!value);
+            }}
+            title={value?'Clicked':'Click'}></Button>
         </View>
-    )
+    );
 }
 
-export default Wolad;
+const Hello=()=>{
+    return(
+        <View>
+            <Text>Hello</Text>
+            <Detials/>
+            <Detials/>
+            <Detials name="Manu"/>
+            <Detials name="Ali"/>
+            <Detials name="thalsi"/>
+        </View>
+    );
+}
+
+export default Hello;
--- README.md
+++ README.md
@@ -26,3 +26,18 @@
 6. npm install -g react-native-cli
 7. npx react-native init AwesomeProject
 8. npx react-native run-android
+
+## 1. component
+- Create component types
+    1. functional component
+    2. class component
+
+## 2. Custom Components
+## 3. Multiple Components
+## 4. Props
+- Props is short for “properties”.
+- Props let you customize React components.
+## 5. State
+- tate is like a component’s personal data storage
+- State is useful for handling data that changes over time 
+- State gives your components memory
