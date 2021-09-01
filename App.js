068adfea9201c5c455ff63d77797c4ee8faa126d
thalsi <thalseehthalsi609@gmail.com>
2021-09-01 18:57:17 +0530

    nativegion

--- App.js
+++ App.js
@@ -1,8 +1,17 @@
 import React from 'react';
-import List from './src/list/list'
+import List from './src/list/list';
+import Home from './src/Home/Home';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+
 const App = () => {
     return (
-        <List />
+        <Stack.Navigator   initialRouteName="Home">
+            <Stack.Screen name="List" component={List} />
+            <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
     );
 }
 
