84f26cbc44d6ef8c763a6074e82ef356e757df3c
thalsi <thalseehthalsi609@gmail.com>
2021-09-02 19:07:33 +0530

    button click nativcation

--- App.js
+++ App.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import List from './src/list/list';
 import Home from './src/Home/Home';
+import { NavigationContainer } from '@react-navigation/native';
+
 import { createStackNavigator } from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
 
-
 const App = () => {
     return (
-        <Stack.Navigator   initialRouteName="Home">
-            <Stack.Screen name="List" component={List} />
-            <Stack.Screen name="Home" component={Home} />
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="List" component={List} />
+          <Stack.Screen name="Home" component={Home} />
         </Stack.Navigator>
+      </NavigationContainer>
     );
 }
-
 
 export default App;
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
-
+import { Text,Button } from 'react-native';
 class Home extends Component {
   render() {
+    
     return (
-      <Text>Hello, I am your cat!</Text>
+      <Text>Hello, I am your cat!</Text>,
+      <Button title="Go to List" onPress={() =>this.props.navigation.navigate('List')} />
     );
   }
 }
