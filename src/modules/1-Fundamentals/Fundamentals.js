021cc6d4fcac94eb4f08c1ac835c089618063e9a
thalsi <thalseehthalsi609@gmail.com>
2021-09-05 13:19:42 +0530

    fandamentals

--- src/modules/1-Fundamentals/Fundamentals.js
+++ src/modules/1-Fundamentals/Fundamentals.js
@@ -1,12 +1,24 @@
 import React,{ Component } from "react";
 import {Text} from 'react-native';
 
+//1.class component
 class Fundamentals extends Component{
     render(){
         return(
-            <Text>Hi Fundamentals..</Text>
+            <Text>Hi class component</Text>,
+            <Functional/>
         )
     }
 }
-
 export default Fundamentals;
+
+
+
+
+
+//2.functional component
+const Functional =()=>{
+    return(
+        <Text>Hi functional component</Text>
+    )
+}
